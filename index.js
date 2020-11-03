d86067cad119416b6b6c7e3a48bc38ebd5208046
kimmjen <wpals814@gmail.com>
2020-11-04 01:17:25 +0900

    third

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
-const mongoose = require('mongoose')
+// application/X-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://kimmjen:a197346852@cluster0.0zms8.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser:true, 
@@ -14,6 +22,19 @@ mongoose.connect('mongodb+srv://kimmjen:a197346852@cluster0.0zms8.mongodb.net/te
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        minlength: 10
+        minlength: 5
     },
     lastname: {
         type: String,
