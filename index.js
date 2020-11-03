2d5e7beb9af480efd1b579e5324a0c383945ecc7
kimmjen <wpals814@gmail.com>
2020-11-04 00:15:11 +0900

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://kimmjen:a197346852@cluster0.0zms8.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser:true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true, 
+    useFindAndModify: false
+}).then(() => console.log('MongoDB Connected.....'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localshot:${port}`)
+})
